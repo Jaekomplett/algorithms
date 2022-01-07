c1df15023635074f7bb988a0dc194d1ff22ac60c
Jaekomplett <wanzekim@gmail.com>
2022-01-07 21:35:12 +0900

    ---

--- algorithm-basic/13_readVertically.js
+++ algorithm-basic/13_readVertically.js
@@ -26,6 +26,7 @@
 // output = readVertically(input);
 // console.log(output); // --> 'hwiolrd'
 
+// method. 1
 function readVertically(arr) {
   // code goes here
 
@@ -60,4 +61,26 @@ function readVertically(arr) {
    return result;
    
    
+}
+
+// method. 2
+function readVertically(arr) {
+  let temp = [];
+  for (let i = 0; i < arr.length; i++) {
+    let str = arr[i];
+    for (let j = 0; j < str.length; j++) {
+      if (temp.length === j) {
+        temp.push(str[j]);
+      } else {
+        temp[j] = temp[j] + str[j];
+      }
+    }
+  }
+
+  let result = '';
+  for (let i = 0; i < temp.length; i++) {
+    result = result + temp[i];
+  }
+
+  return result;
 }
